64355ac3f84811e010fc46ad5b2043f00000d5b7
RamaSai2519 <rchandik@gitam.in>
2024-04-10 14:01:40 +0530

    fetch restage

--- src/components/RecentBlogPosts/RecentBlogPosts.js
+++ src/components/RecentBlogPosts/RecentBlogPosts.js
@@ -1,6 +1,4 @@
-// RecentBlogPosts.js
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import './RecentBlogPosts.css'; // Import CSS file for styling
 import { Link } from 'react-router-dom';
 
@@ -10,9 +8,15 @@ const RecentBlogPosts = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get('http://15.206.127.248/api/blogs')
+    fetch('http://15.206.127.248/api/blogs')
       .then(response => {
-        setBlogs(response.data);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setBlogs(data);
         setLoading(false);
       })
       .catch(error => {
@@ -35,13 +39,11 @@ const RecentBlogPosts = () => {
       <div className="blog-grid">
         {blogs.map(blog => (
           <div className="blog-post" key={blog.id }>
-          <Link to={`/blog/${blog.id}`} className='blog-post-link'>
-          
-            <img src={blog.image} alt={blog.description}></img>
-            <h3>{blog.title}</h3>
-            <p>{blog.description}</p>
-          
-          </Link>
+            <Link to={`/blog/${blog.id}`} className='blog-post-link'>
+              <img src={blog.image} alt={blog.description}></img>
+              <h3>{blog.title}</h3>
+              <p>{blog.description}</p>
+            </Link>
           </div>
         ))}
       </div>
