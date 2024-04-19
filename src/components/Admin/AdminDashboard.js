2ac4a2b19f89b42d6a96709f61f66c02398e701a
RamaSai2519 <rchandik@gitam.in>
2024-04-19 15:33:57 +0530

    push

--- src/components/Admin/AdminDashboard.js
+++ src/components/Admin/AdminDashboard.js
@@ -1,11 +1,11 @@
-// AdminDashboard.js
+// src/components/Admin/AdminDashboard.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import CallGraph from './CallGraph';
 import ExpertGraph from './ExpertGraph';
 
 const AdminDashboard = () => {
-  const [totalCalls, setTotalCalls] = useState(0);
+  const [totalCalls, setTotalCalls] = useState([]);
   const [successfulCalls, setSuccessfulCalls] = useState(0);
   const [totalUsers, setTotalUsers] = useState(0);
   const [onlineSaarthis, setOnlineSaarthis] = useState([]);
@@ -14,46 +14,30 @@ const AdminDashboard = () => {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-  
   const fetchData = async () => {
     try {
-      const callsResponse = await axios.get('/api/calls');
-      const usersResponse = await axios.get('/api/users');
-      const expertsResponse = await axios.get('/api/experts');
-  
-      // Check if any of the responses are empty
-      if (!callsResponse.data || !usersResponse.data || !expertsResponse.data ) {
-        throw new Error('Empty response received');
-      }
-  
-      // Set state with the fetched data
-      setTotalCalls(callsResponse.data.total_calls);
-      setSuccessfulCalls(callsResponse.data.successful_calls);
-      setTotalUsers(usersResponse.data.users.length);
-      setOnlineSaarthis(expertsResponse.data.total_online_saarthis);
+      const response = await axios.get('/api/calls');
+      const callsData = response.data;
+      setTotalCalls(callsData);
+      setSuccessfulCalls(calculateTotalCalls(callsData));
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
-  
-  
+
+  const calculateTotalCalls = (callsData) => {
+    // Calculate the total number of calls
+    return callsData.length;
+  };
 
   return (
     <div>
       <h2>Admin Dashboard</h2>
       <div>
-        <p>Total Calls: {totalCalls}</p>
+        <p>Total Calls: {totalCalls.length}</p>
         <p>Total Successful Calls: {successfulCalls}</p>
         <p>Total Users: {totalUsers}</p>
         <p>Online Saarthis: {onlineSaarthis.length}</p>
-        <ul>
-          {totalCalls.map(call => (
-            <li key={call._id}>{call.initiatedTime} - {call.status}</li>
-          ))}
-        </ul>
       </div>
       <CallGraph />
       <ExpertGraph />
